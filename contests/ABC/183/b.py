a83e31368f7b0de54f39b3dede8e75cc6a0d3904
1b0325h <melano@1b0325h.p001.jp>
2021-08-31 22:14:19 +0900

    B - Billiards

---
+++ contests/ABC/183/b.py
@@ -0,0 +1,7 @@
+# https://atcoder.jp/contests/abc183/submissions/25166941
+
+# %%
+Sx, Sy, Gx, Gy = map(int, input().split())
+
+print((Sx*Gy + Gx*Sy) / (Sy+Gy))
+# %%
--- problems/abc/192/a.py
+++
@@ -1,7 +0,0 @@
-# https://atcoder.jp/contests/abc192/submissions/25146684
-
-# %%
-X = int(input())
-
-print(100 - X % 100)
-# %%
