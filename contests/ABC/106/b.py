ba9150d4b0036578ed33216550a78bc13f81f99d
1b0325h <melano@1b0325h.p001.jp>
2021-09-01 22:41:59 +0900

    B - 105

---
+++ contests/ABC/106/b.py
@@ -0,0 +1,21 @@
+# https://atcoder.jp/contests/abc106/submissions/25462539
+
+# %%
+def divisors(n):
+    x, i = set(), 1
+    while i**2 <= n:
+        if not n % i:
+            x.add(i)
+            x.add(n//i)
+        i += 1
+    return sorted(x)
+
+N = int(input())
+
+ans = 0
+for i in range(1, N+1, 2):
+    if len(divisors(i)) == 8:
+        ans += 1
+
+print(ans)
+# %%
