aea01ee326a004187848f7d3bcae4e4e07acddc0
1b0325h <melano@1b0325h.p001.jp>
2021-09-21 21:26:17 +0900

    C - Sum of gcd of Tuples (Easy)

--- contests/ABC/162/c.py
+++ contests/ABC/162/c.py
@@ -1,19 +1,19 @@
-# https://atcoder.jp/contests/abc162/submissions/25117035
+# https://atcoder.jp/contests/abc162/submissions/26023783
 
 # %%
 from functools import reduce
 from itertools import product
 
-def gcd(x, y):
+def igcd(x, y):
     while y:
-        x, y = y, x % y
+        x, y = y, x%y
     return x
 
 K = int(input())
 
 ans = 0
 for i in product(range(1, K+1), repeat=3):
-    ans += reduce(gcd, i)
+    ans += reduce(igcd, i)
 
 print(ans)
 # %%
