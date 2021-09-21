2b2de589d796d8e48fdc1c8e6e78ffc15f9987d5
1b0325h <melano@1b0325h.p001.jp>
2021-09-21 21:29:34 +0900

    D - Base n

--- contests/ABC/192/d.py
+++ contests/ABC/192/d.py
@@ -1,14 +1,14 @@
-# https://atcoder.jp/contests/abc192/submissions/25152551
+# https://atcoder.jp/contests/abc192/submissions/26024471
 
 # %%
-def base_10(n, base):
-    x, c = list(n), 0
+def decimal(s, b):
+    x, c = list(s), 0
     while x:
-        c *= base
+        c *= b
         c += int(x.pop(0))
     return c
 
-def binary_search(ng, ok, f):
+def binary_search(ok, ng, f):
     while abs(ok-ng) > 1:
         mid = (ok+ng) // 2
         if f(mid):
@@ -21,10 +21,10 @@ X = input()
 M = int(input())
 
 def f(n):
-    return base_10(X, n) <= M
+    return decimal(X, n) <= M
 
 if len(X) == 1 and int(X) <= M:
     print(1)
 else:
-    print(max(binary_search(M+1, 0, f) - int(max(X)), 0))
+    print(max(binary_search(0, M+1, f) - int(max(X)), 0))
 # %%
