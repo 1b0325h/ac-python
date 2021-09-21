eac65c050c03a9d9aa2f1e37f21cebce18a96c38
1b0325h <melano@1b0325h.p001.jp>
2021-09-21 21:31:05 +0900

    C - Landslide

--- contests/PAST/002/c.py
+++ contests/PAST/002/c.py
@@ -1,4 +1,4 @@
-# https://atcoder.jp/contests/past202004-open/submissions/25513390
+# https://atcoder.jp/contests/past202004-open/submissions/25698344
 
 # %%
 N = int(input())
@@ -9,9 +9,9 @@ for i in range(N-2, -1, -1):
         if S[i][j] == "#":
             if S[i+1][j-1] == "X":
                 S[i][j] = "X"
-            elif S[i+1][j] == "X":
+            if S[i+1][j] == "X":
                 S[i][j] = "X"
-            elif S[i+1][j+1] == "X":
+            if S[i+1][j+1] == "X":
                 S[i][j] = "X"
 
 print(*["".join(i) for i in S], sep="\n")
