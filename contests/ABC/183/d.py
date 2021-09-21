dc004bb03808ff1286e26a59ec683ab361d3c43e
1b0325h <melano@1b0325h.p001.jp>
2021-09-21 21:26:50 +0900

    D - Water Heater

--- contests/ABC/183/d.py
+++ contests/ABC/183/d.py
@@ -1,12 +1,15 @@
-# https://atcoder.jp/contests/abc183/submissions/25169397
+# https://atcoder.jp/contests/abc183/submissions/26024118
 
 # %%
-def imos1(start, end, add):
-    table = [0] * (max(end)+2)
-    for i in range(len(start)):
-        table[start[i]] += add[i]
-        table[end[i]] -= add[i]
-    for i in range(1, max(end)+2):
+def imos(l, r, add, p=0):
+    if isinstance(add, int):
+        add = [add] * len(l)
+    r = [i+p for i in r]
+    table = [0] * (max(r)+2)
+    for i in range(len(l)):
+        table[l[i]] += add[i]
+        table[r[i]] -= add[i]
+    for i in range(1, max(r)+2):
         table[i] += table[i-1]
     return table
 
@@ -14,5 +17,5 @@ N, W = map(int, input().split())
 STP = [map(int, input().split()) for _ in range(N)]
 S, T, P = [list(_) for _ in zip(*STP)]
 
-print("Yes" if max(imos1(S, T, P)) <= W else "No")
+print("Yes" if max(imos(S, T, P)) <= W else "No")
 # %%
