f9c53cea45616c6b2c3255437017e967c5ce6fc3
1b0325h <melano@1b0325h.p001.jp>
2021-09-21 21:25:00 +0900

    C - Prison

--- contests/ABC/127/c.py
+++ contests/ABC/127/c.py
@@ -1,12 +1,15 @@
-# https://atcoder.jp/contests/abc127/submissions/25169324
+# https://atcoder.jp/contests/abc127/submissions/26022371
 
 # %%
-def imos1(start, end, add):
-    table = [0] * (max(end)+2)
-    for i in range(len(start)):
-        table[start[i]] += add[i]
-        table[end[i]] -= add[i]
-    for i in range(1, max(end)+2):
+def imos(l, r, add, p=0):
+    if isinstance(add, int):
+        add = [add] * len(l)
+    r = [i+p for i in r]
+    table = [0] * (max(r)+2)
+    for i in range(len(l)):
+        table[l[i]] += add[i]
+        table[r[i]] -= add[i]
+    for i in range(1, max(r)+2):
         table[i] += table[i-1]
     return table
 
@@ -14,5 +17,5 @@ N, M = map(int, input().split())
 LR = [map(int, input().split()) for _ in range(M)]
 L, R = [list(_) for _ in zip(*LR)]
 
-print(imos1(L, [i+1 for i in R], [1]*M).count(M))
+print(imos(L, R, 1, 1).count(M))
 # %%
