bccf3111dfbfc1bcd2ab0dd69805ae3288d8895b
1b0325h <melano@1b0325h.p001.jp>
2021-09-23 06:22:24 +0900

    A - AtCoder Quiz 2

---
+++ contests/ABC/219/a.py
@@ -0,0 +1,14 @@
+# https://atcoder.jp/contests/abc219/submissions/26038113
+
+# %%
+X = int(input())
+
+if X < 40:
+    print(40-X)
+elif X < 70:
+    print(70-X)
+elif X < 90:
+    print(90-X)
+else:
+    print("expert")
+# %%
